1971ffa5ac3da2bf6e686745ebef7c3da627fdd2
Tian-Shi Xu <994876761@qq.com>
2026-04-06 22:21:07 +0800

    .

---
+++ verl/examples/grpo_trainer/run_qwen2_5_7b_grpo.sh
@@ -0,0 +1,61 @@
+set -x
+
+# profiling configuration
+PROFILE_STEPS="[2,4]"
+PROFILE_RANKS_ALL=False
+DISCRETE=True
+PROFILE_RANKS="[1,2]"cat run_qwen2_5_7b_grpo_discrete_prof_npu.sh | grep model.path
+SAVE_PATH="/workspace/profile_data"
+LEVEL="level1"
+CONTENTS="['npu','cpu']"
+ANALYSIS=True
+train_batch_size=128
+ppo_mini_batch_size=32
+CUDA_VISIBLE_DEVICES=0,3
+
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=/workspace/data/math_train_fixed.parquet \
+    data.val_files=/workspace/data/math_test_fixed.parquet \
+    data.train_batch_size=$train_batch_size \
+    data.max_prompt_length=1024 \
+    data.max_response_length=1024 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    +model.torch_dtype=bfloat16 \
+    actor_rollout_ref.model.path=/workspace/Qwen2.5-7B \
+    +actor_rollout_ref.actor.fsdp_config.mixed_precision.param=bfloat16 \
+    +actor_rollout_ref.actor.fsdp_config.mixed_precision.reduce=bfloat16 \
+    +actor_rollout_ref.actor.fsdp_config.mixed_precision.buffer=bfloat16 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.model.use_remove_padding=False \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    +actor_rollout_ref.rollout.max_model_len=2048 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=$ppo_mini_batch_size \
+    actor_rollout_ref.rollout.name=sglang \
+    actor_rollout_ref.rollout.mode=async \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
+    actor_rollout_ref.rollout.n=16 \
+    actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    trainer.logger=wandb \
+    trainer.project_name='verl_grpo_math' \
+    trainer.experiment_name='qwen2_5_7b_grpo' \
+    trainer.default_local_dir=/tmp/verl_outputs \
+    trainer.n_gpus_per_node=2 \
+    trainer.nnodes=1 \
+    trainer.save_freq=-1 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=1 \
+    trainer.device=cuda \
+    reward_model.strategy=naive \
+    +reward_model.num_examine=5\
+    global_profiler.tool=None \
+    global_profiler.steps=$PROFILE_STEPS \
+    global_profiler.save_path=$SAVE_PATH
